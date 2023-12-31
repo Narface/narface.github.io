6d6a53564d19953fc2b53b3501c9178a7e6e53e8
Fran <thenarface@gmail.com>
2023-12-31 17:11:46 +0100

    favicon

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704026567';
+const cacheName = 'chirpy-1704030589';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
